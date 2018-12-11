ae33c49321b679811880002706bf38af5968f2e8
Carlos Cordeiro <carloscordeiroconsultor@gmail.com>
2018-12-10 21:59:11 -0300

    modificado cardinalidade e nome das tabelas de treino

--- src/model/Treino.java
+++ src/model/Treino.java
@@ -8,6 +8,7 @@ package model;
 
 import java.util.List;
 import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -15,8 +16,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 /**
@@ -51,18 +52,18 @@ public class Treino {
 
 	    
 	    @OneToOne 
-	    @JoinColumn(name = "cod_professor" )
+	    @JoinColumn(name = "professor_" )
 	    private Professor professor;   
 	    
 	    @Column(length = 6)
 	    private String intensidade;    
 	    
 
-	    @OneToMany (fetch = FetchType.EAGER)
+	    @ManyToMany (fetch = FetchType.EAGER)
 	    private List<Exercicio> exercicios;
 	    
-	    @ManyToOne
-	    @JoinColumn(name = "cod_aluno" )
+	    @ManyToOne		
+	    @JoinColumn(name = "aluno_id" )
 	    private Aluno aluno;
 	    
 	    @Column
